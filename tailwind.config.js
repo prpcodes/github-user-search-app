8f78d716a96ad8d67fbbef51eb53d41f62471acb
prpwien <mail@prp.wien>
2022-02-14 14:50:39 +0100

    💄 added template styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
     extend: {},
     colors: {
-      blue: "#0079ff",
-      "blue-dark": "#4b6a9b",
-      "blue-darker": "1E2A47",
-      "blue-very-dark": "#141D2F",
-      gray: "#697c9a",
-      "gray-dark": "#2b3442",
-      white: "#FFFFFF",
-      "white-light": "#fefefe",
-      "white-dark": "#f6f8ff",
+      blue: '#0079ff',
+      'blue-dark': '#4b6a9b',
+      'blue-darker': '#1E2A47',
+      'blue-very-dark': '#141D2F',
+      gray: '#697c9a',
+      'gray-dark': '#2b3442',
+      white: '#FFFFFF',
+      'white-light': '#fefefe',
+      'white-dark': '#f6f8ff',
     },
   },
   plugins: [],
