3fab655d38fb7513949f89322dfb8438f3278c22
Philipp R. Proksch <mail@prp.wien>
2022-02-23 15:45:09 +0000

    ✨ added data fetching

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,12 +1,49 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Image from "next/image";
+import { UserContext } from "../context/UserContext";
 
-export default function Home() {
+import { useContext, useState } from "react";
+
+export default function SearchBar() {
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const { setUser } = useContext(UserContext);
+
+  function handleSearch(username) {
+    setLoading(true);
+    fetch(`https://api.github.com/users/${username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }
+
   return (
     <div className="sm:max-w-sceen flex bg-white dark:bg-blue-darker rounded">
-      <Image src={'/icon-search.svg'} alt="Search Icon" width={20} height={20} />
-      <input className="bg-white dark:bg-blue-darker" defaultValue="Search GitHub username" />
-      <button className="bg-blue rounded text-white p">Search</button>
+      <Image
+        src={"/icon-search.svg"}
+        alt="Search Icon"
+        width={20}
+        height={20}
+      />
+      <input
+        className="bg-white dark:bg-blue-darker"
+        type="text"
+        name="username"
+        placeholder="Search GitHub username..."
+        required
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button
+        className="bg-blue rounded text-white"
+        onClick={(e) => {
+          e.preventDefault();
+          handleSearch(username);
+        }}
+        disabled={loading}
+      >
+        {loading ? "Loading ..." : "Search"}
+      </button>
     </div>
   );
 }
