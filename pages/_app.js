c6bb7f59a40749089d4da1d93699bc1cbcf4d72a
Philipp R. Proksch <mail@prp.wien>
2022-02-23 15:47:24 +0000

    💄 moved styling to <body>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import { ThemeProvider } from 'next-themes';
-import '../styles/globals.css';
+//import { ThemeProvider } from 'next-themes';
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component className="bg-white-dark text-blue-dark dark:bg-blue-very-dark dark:text-white h-screen" {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
