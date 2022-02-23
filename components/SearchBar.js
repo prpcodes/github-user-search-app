a5596f9c594d4439b1e101de07deeb4a1e16fd5e
Philipp R. Proksch <mail@prp.wien>
2022-02-23 17:38:52 +0000

    ✨ added form and div

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -19,15 +19,17 @@ export default function SearchBar() {
   }
 
   return (
-    <div className="sm:max-w-sceen flex bg-white dark:bg-blue-darker rounded">
-      <Image
-        src={"/icon-search.svg"}
-        alt="Search Icon"
-        width={20}
-        height={20}
-      />
+    <form className="py-2 px-2 bg-blue-darker rounded-2xl flex justify-between w-full">
+      <div className="m-auto">
+        <Image
+          src={"/icon-search.svg"}
+          alt="Search Icon"
+          width={20}
+          height={20}
+        />
+      </div>
       <input
-        className="bg-white dark:bg-blue-darker"
+        className="bg-blue-darker "
         type="text"
         name="username"
         placeholder="Search GitHub username..."
@@ -35,7 +37,7 @@ export default function SearchBar() {
         onChange={(e) => setUsername(e.target.value)}
       />
       <button
-        className="bg-blue rounded text-white"
+        className="bg-blue rounded-xl text-white h-12 w-20"
         onClick={(e) => {
           e.preventDefault();
           handleSearch(username);
@@ -44,6 +46,6 @@ export default function SearchBar() {
       >
         {loading ? "Loading ..." : "Search"}
       </button>
-    </div>
+    </form>
   );
 }
